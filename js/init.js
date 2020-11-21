42ca8ead82f89f85df86f469c6fd23fe6d753c52
Grace Kuo <gkuo.github@gmail.com>
2020-11-21 11:20:20 -0700

    fixed javascript for materialboxed

--- js/init.js
+++ js/init.js
@@ -5,7 +5,6 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".materialboxed");
-  var instances = M.Materialbox.init(elems, options);
+$(document).ready(function () {
+  $(".materialboxed").materialbox();
 });
