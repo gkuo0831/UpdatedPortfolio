5496fa8e8395865f77e1a89c6d3b2ab23e05c76a
Grace Kuo <gkuo.github@gmail.com>
2020-11-21 11:32:35 -0700

    created js for next and prev buttons

--- js/init.js
+++ js/init.js
@@ -10,6 +10,17 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    $(".carousel")
-      .carousel({ fullWidth: true })
-      .css("height", $(window).height());
+  $(".carousel")
+    .carousel({ fullWidth: true })
+    .css("height", $(window).height());
+
+  // for next slide
+  $(".next").click(function () {
+    $(".carousel").carousel("next");
+  });
+
+  // for prev slide
+  $(".prev").click(function () {
+    $(".carousel").carousel("prev");
+  });
+});
